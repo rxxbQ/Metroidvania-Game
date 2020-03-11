df8bc604adc4ff5e6cd2f487c6767e153c76128c
rxxbQ <43527247+rxxbQ@users.noreply.github.com>
2020-03-11 15:30:15 -0400

    Add files via upload

--- Script/SavePoint.cs
+++ Script/SavePoint.cs
@@ -11,5 +11,8 @@ public class SavePoint : MonoBehaviour, IInteraction
     {
         Player.Instance.startPosition = transform.position;
         gameObject.GetComponent<SpriteRenderer>().sprite = lit;
+        Player.Instance.hp.CurrentValue = Player.Instance.hp.MaxValue;
+        Player.Instance.mana.CurrentValue = Player.Instance.mana.MaxValue;
+        Player.Instance.stamina.CurrentValue = Player.Instance.stamina.MaxValue;
     }
 }
