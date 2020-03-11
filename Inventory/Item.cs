9f436612d98b1f0d5821e449202704f63c72e4e6
rxxbQ <43527247+rxxbQ@users.noreply.github.com>
2020-03-11 00:03:09 -0400

    Add files via upload

---
+++ Inventory/Item.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string type;
+    public string des;
+    public Sprite icon;
+
+    public void ItemUsage()
+    {
+        if (type == "Powerup")
+        {
+            if (id == 2)
+            {
+                Player.Instance.hp.MaxValue = 0.5f * Player.Instance.hp.MaxValue;
+                Player.Instance.hp.CurrentValue = Player.Instance.hp.MaxValue;
+                Player.Instance.attackDamage = 2 * Player.Instance.attackDamage;
+            }
+            if (id == 3)
+            {
+                Player.Instance.attackDamage = (int)Mathf.Round(1.25f * Player.Instance.attackDamage);
+            }
+            if (id == 4)
+            {
+                Player.Instance.defence = (int)Mathf.Round(1.25f * Player.Instance.defence);
+            }
+        }
+        if (type == "Potion")
+        {
+            if (id == 0)
+            {
+                Player.Instance.hp.CurrentValue += 15;
+            }
+            if (id == 1)
+            {
+                Player.Instance.mana.CurrentValue += 15;
+            }
+        }
+    }
+}
