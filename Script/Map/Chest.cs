6f74ce797b84ac69d548ee5404628dd4038320a7
rxxbQ <43527247+rxxbQ@users.noreply.github.com>
2020-04-03 19:37:39 -0400

    Add files via upload

--- Script/Map/Chest.cs
+++ Script/Map/Chest.cs
@@ -8,6 +8,9 @@ public class Chest : MonoBehaviour,IInteraction
     private Sprite openedChest;
     private bool opened = false;
 
+    [SerializeField]
+    private bool dropLeft;
+
     public void Interact()
     {
         GetComponent<SpriteRenderer>().sprite = openedChest;
@@ -16,6 +19,18 @@ public class Chest : MonoBehaviour,IInteraction
         {
             FindObjectOfType<AudioManager>().Play("openChest");
             GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + 0.45f, transform.position.z);
+
+            for (int i = 0; i < Random.Range(10.0f, 15.0f); i++)
+            {
+                if (dropLeft)
+                {
+                    GameObject coin = (GameObject)Instantiate(GameManager.Instance.Coin, new Vector3(transform.position.x-2, transform.position.y-1), Quaternion.identity);
+                }
+                else
+                {
+                    GameObject coin = (GameObject)Instantiate(GameManager.Instance.Coin, new Vector3(transform.position.x +2, transform.position.y - 1), Quaternion.identity);
+                }
+            }
         }
         opened = true;
     }
