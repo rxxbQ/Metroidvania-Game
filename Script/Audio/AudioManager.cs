4949f2cf416a4868cb8c848eef815b33342c0d8b
rxxbQ <43527247+rxxbQ@users.noreply.github.com>
2020-04-03 19:35:57 -0400

    Add files via upload

---
+++ Script/Audio/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.audioClip;
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Play("theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.audioSource.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found");
+            return;
+        }
+        s.audioSource.Stop();
+    }
+}
